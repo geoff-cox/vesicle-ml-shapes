926a249e68c2a93412e102fce2820f26f33db3ca
geoff-cox <geoffreywcox@gmail.com>
2025-10-17 18:11:59 -0400

    Major-refactoring

--- scripts/run_debug_sweep.m
+++
@@ -1,9 +0,0 @@
-% --- run_debug_sweep.m
-run('bootstrap.m');
-
-% Open the driver and force debug mode (short run) at runtime:
-sim_driver_quad_tree;   % uses sim.debug.short = true inside the driver
-
-% This should:
-% - create SimResults/<DATE>/run_<timestamp>/
-% - emit OPfile.txt (log), catalog.mat (table) and a solutions/ dir
